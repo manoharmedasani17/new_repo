344a5a2ec382291fe16afca3eba89e688e25729a
Manohar <manohar.babu8@yahoo.com>
2021-04-25 15:10:00 +0530

    start develop branch

---
+++ sub.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+	int a,b;
+
+	printf("enter the a value:\n");
+	scanf("%d\n",&a);
+
+	printf("enter the b value:\n");
+	scanf("%d\n",&b);
+
+	int c = a-b;
+
+	printf("the sum value is:%d",c);
+}
